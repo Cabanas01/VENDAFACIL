66d6081727ac1225296e8bdbd369b1e7a69193f6
GUILHERME CABANAS <guicabanas13@gmail.com>
2026-01-28 20:33:05 +0000

    NEXT_PUBLIC_SUPABASE_URL=https://htlrtgzhzvvsxqyzkevi.supabase.co NEXT_P

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  "https://htlrtgzhzvvsxqyzkevi.supabase.co",
+  "sb_publishable_T8KSbI65LTeEkXi8GvM2wg_JQijyvBn",
   {
     auth: {
       persistSession: true,
